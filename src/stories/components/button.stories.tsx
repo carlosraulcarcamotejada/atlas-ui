5c0b4064fe6c79d334c33197a0403cda50ca67e0
Carlos Raúl Cárcamo Tejada <carlosraulcarcamotejada@gmail.com>
2025-04-21 11:12:53 -0600

    first commit

---
+++ src/stories/components/button.stories.tsx
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "@/components/button";
+
+const meta: Meta<typeof Button> = {
+  title: "Components/Button",
+  component: Button,
+  tags: ["autodocs"],
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    variant: "default",
+    children: "Click me",
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    variant: "destructive",
+    children: "Click me",
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    variant: "ghost",
+    children: "Click me",
+  },
+};
+
+export const Link: Story = {
+  args: {
+    variant: "link",
+    children: "Click me",
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    variant: "outline",
+    children: "Outline Button",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: "secondary",
+    children: "Click me",
+  },
+};
