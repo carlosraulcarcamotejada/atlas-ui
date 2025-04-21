27750fc536f5ff98f7abb4b17e8d5c0cefbd0362
Carlos Raúl Cárcamo Tejada <carlosraulcarcamotejada@gmail.com>
2025-04-21 12:37:25 -0600

    setud done

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,30 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/components/index.ts"),
+      name: "Atlas-UI",
+      fileName: "atlas-ui",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  plugins: [react(), tailwindcss(), dts({ rollupTypes: true })],
 });
