047c0d5c2a280c15d7a57ce7610e1b54c261fb21
Carlos Raúl Cárcamo Tejada <carlosraulcarcamotejada@gmail.com>
2025-07-11 16:21:27 -0600

    refactoring

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@/components/ui/typography";
+
+function App() {
+  return (
+    <div className="w-full min-h-screen grid place-content-center">
+      <Typography as="h1" variant="heading-xl" weight="bold">
+        Atlas UI Library
+      </Typography>
+    </div>
+  );
+}
+
+export { App };
